c63f84d10ee8fc5e18390b775e2117763e9b20eb
momischmiddi <Moritzschmidt1995@gmail.com>
2019-11-14 14:01:29 +0100

    Code cleanup

--- src/test/scala/traits/TestBase.scala
+++ src/test/scala/traits/TestBase.scala
@@ -8,18 +8,19 @@ trait TestBase {
   def createTestObjects(difficulty: Difficulty, useTestBombs: Boolean): (GameFieldCreator, List[(Int, Int)], GameField) = {
     val creator = new GameFieldCreator
     val bombs: List[(Int, Int)] = if(useTestBombs) {
-      val bombs: List[(Int, Int)] = List(
-        (0,4), (0, 5), (0, 7), (1, 7), (2, 3),
-        (4, 4), (5, 5 ), (6, 3), (6, 5), (7, 3)
-      )
-      bombs
+      createTestBombs()
     } else {
       creator.createRandomBombLocations(difficulty)
     }
 
-    val gameField = creator.createGameField(difficulty, bombs)
+    (creator, bombs, creator.createGameField(difficulty, bombs))
+  }
 
-    (creator, bombs, gameField)
+  private def createTestBombs(): List[(Int, Int)] = {
+    List(
+      // DO NOT MODIFY! Tests will fail if you do.
+      (0,4), (0, 5), (0, 7), (1, 7), (2, 3), (4, 4), (5, 5 ), (6, 3), (6, 5), (7, 3)
+    )
   }
 
 }
