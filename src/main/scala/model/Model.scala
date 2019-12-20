32223b6f0991bae6846843a738d32a9d9b7281ba
momischmiddi <Moritzschmidt1995@gmail.com>
2019-12-20 23:57:23 +0100

    Removed dead code

--- src/main/scala/model/Model.scala
+++ src/main/scala/model/Model.scala
@@ -18,20 +18,15 @@ class Model(val difficulty: Difficulty) extends Subject {
       val replaced = fieldMatrix.replaceField(x, y, newField)
 
       val gameDone = isGameDone(replaced.fields)
-      val gameWon = isGameWon(replaced.fields, difficulty)
 
       if (flag) {
         fireFieldUpdated(replaced, GameStatus.InProgress)
       } else if (!gameDone) {
-        if (gameWon) {
-          fireFieldUpdated(replaced, GameStatus.Won)
+        val updated = expand(selectedField, replaced)
+        if (isGameWon(updated.fields, difficulty)) {
+          fireFieldUpdated(updated, GameStatus.Won)
         } else {
-          val updated = expand(selectedField, replaced)
-          if (isGameWon(updated.fields, difficulty)) {
-            fireFieldUpdated(updated, GameStatus.Won)
-          } else {
-            fireFieldUpdated(updated, GameStatus.InProgress)
-          }
+          fireFieldUpdated(updated, GameStatus.InProgress)
         }
       } else {
         fireFieldUpdated(createGameLostField(x, y, replaced), GameStatus.Lost)
@@ -60,33 +55,6 @@ class Model(val difficulty: Difficulty) extends Subject {
       ))((r, n) => expand(n, r))
   }
 
-  def openNeighboursRecursively(x: Int, y: Int, hasSurroundingBombs: Boolean, fieldMatrix: FieldMatrix): FieldMatrix = {
-    val field = fieldMatrix.get(y, x)
-
-    val neighbours = if(hasSurroundingBombs) {
-      // If the field has a number on it, diagonal neighbours should be ignored
-      // Its a diagonal field, if the manhattan distance is > 1
-      getNeighbours(y, x, fieldMatrix).filter(f => calculateManhattanDistance(field, f) > 1)
-    } else {
-      getNeighbours(y, x, fieldMatrix)
-    }
-
-    for(neighbour <- neighbours) {
-      val (nX, nY) = (neighbour.xLocation, neighbour.yLocation)
-
-      // Recursively open all neighbours, which are no bombs and have no numbers on it.
-      if(!neighbour.isBomb && !neighbour.isOpened && field.surroundingBombs < 1) {
-        val bombAmount = getSurroundingBombAmount(fieldMatrix, nX, nY)
-        val toReplace = neighbour.open().setBombs(bombAmount)
-        val replaced = fieldMatrix.replaceField(nX, nY, toReplace)
-
-        openNeighboursRecursively(nX, nY, bombAmount > 0, replaced)
-      }
-    }
-
-    fieldMatrix
-  }
-
   def createGameLostField(x: Int, y: Int, fieldMatrix: FieldMatrix): FieldMatrix =
     fieldMatrix.fields.flatten.filter(f => f.isBomb)
       .foldLeft(fieldMatrix)((fieldMatrix, bomb) => fieldMatrix.replaceField(
@@ -95,23 +63,16 @@ class Model(val difficulty: Difficulty) extends Subject {
       x, y, Field(x, y, true, false, true, 0, true)
     )
 
-  def ignoreClick(field: Field, flag: Boolean, fields: Vector[Vector[Field]], difficulty: Difficulty) : Boolean =
-    field.isOpened || (field.isFlagged && !flag) || isGameDone(fields) || isGameWon(fields, difficulty)
+  def ignoreClick(field: Field, flag: Boolean, fields: Vector[Vector[Field]], difficulty: Difficulty) : Boolean = field.isOpened || (field.isFlagged && !flag) || isGameDone(fields) || isGameWon(fields, difficulty)
 
-  def isGameWon(fields: Vector[Vector[Field]], difficulty: Difficulty) : Boolean =
-    fields.flatten.filter(!_.isBomb).count(_.isOpened) == (difficulty._1 * difficulty._2) - difficulty._3
+  def isGameWon(fields: Vector[Vector[Field]], difficulty: Difficulty) : Boolean = fields.flatten.filter(!_.isBomb).count(_.isOpened) == (difficulty._1 * difficulty._2) - difficulty._3
 
   def isGameDone(fields: Vector[Vector[Field]]) : Boolean =  fields.flatten.filter(_.isBomb).count(_.isOpened) > 0
 
-  def getSurroundingBombAmount(fieldMatrix: FieldMatrix, x: Int, y: Int) : Int =
-    getNeighbours(y, x, fieldMatrix).count(_.isBomb)
-
-  def calculateManhattanDistance(original: Field, toCompare: Field): Int =
-    Math.abs(original.xLocation - toCompare.xLocation) +
-      Math.abs(original.yLocation - toCompare.yLocation)
+  def getSurroundingBombAmount(fieldMatrix: FieldMatrix, x: Int, y: Int) : Int = getNeighbours(y, x, fieldMatrix).count(_.isBomb)
 
   def getNeighbours(y: Int, x: Int, fieldMatrix: FieldMatrix) : List[Field] = {
-    val res = List(
+    List(
       fieldMatrix.get(y, x + 1),
       fieldMatrix.get(y + 1, x),
       fieldMatrix.get(y, x - 1),
@@ -121,6 +82,5 @@ class Model(val difficulty: Difficulty) extends Subject {
       fieldMatrix.get(y + 1, x - 1),
       fieldMatrix.get(y - 1, x - 1),
     ).filter(_ != null)
-    res
   }
 }
--- src/main/scala/observerpattern/Subject.scala
+++ src/main/scala/observerpattern/Subject.scala
@@ -8,9 +8,6 @@ trait Subject {
   private var observers: List[Observer] = Nil
 
   def addGameListener(observer: Observer): Unit = observers = observer :: observers
-  def getGameListeners(): List[Observer] = observers
-  def removeGameListener(observer: Observer): Unit= observers = observers diff List(observer)
-  def removeAllGameListeners(): Unit = observers = observers.diff(observers)
   def fireFieldUpdated(fieldMatrix: FieldMatrix, gameStatus: GameStatus): Unit = observers.foreach(_.gameFieldUpdated(fieldMatrix, gameStatus))
 
 }
